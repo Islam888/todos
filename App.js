f0fa491d16b514ea32e080f7aa373675910eace8
Islam <islam.sayed8@gmail.com>
2018-09-11 21:15:09 +0200

    project initiation

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text } from 'react-native';
+import EventList from './EventList'
+
+export default class App extends React.Component {
+  render() {
+    return (
+        <EventList />
+    );
+  }
+}
+
+
